c1a1223f7f3400cf392751ee46874d469d3290a3
FabioPin <fajos.pinheiro@hotmail.com>
2023-10-27 22:40:16 +0100

    added Topic

--- src/features/topics/Topic.js
+++ src/features/topics/Topic.js
@@ -3,10 +3,12 @@ import { useSelector } from "react-redux";
 import { Link, useParams, Navigate } from "react-router-dom";
 import ROUTES from "../../app/routes";
 // import selectors
+import { selectAllTopics } from "./topicsSlice";
+import { selectAllQuizzes } from "../quizzes/quizzesSlice";
 
 export default function Topic() {
-  const topics = {};  // replace with selector
-  const quizzes = {}; // replace with selector
+  const topics = useSelector(selectAllTopics);  // replace with selector
+  const quizzes = useSelector(selectAllQuizzes); // replace with selector
   const { topicId } = useParams();
   const topic = topics[topicId];
 
